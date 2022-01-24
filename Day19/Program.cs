719d0a3c282032ebb6f2d06edd52947a36f58e6a
Mateusz Bąkała <maba@netcompany.com>
2022-01-24 10:20:40 +0100

    Day 19

---
+++ Day19/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day19
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var elvesCount = int.Parse(File.ReadAllText("input.txt"));
+            Part1(elvesCount);
+            Part2(elvesCount);
+        }
+
+        private static void Part1(int elvesCount)
+        {
+            Console.WriteLine(FlawiusProblem(elvesCount));
+        }
+
+        private static int FlawiusProblem(int elvesCount)
+        {
+            int[] remaining = new int[elvesCount+1];
+            remaining[1] = remaining[2] = 1;
+            for(int n = 3; n <= elvesCount; n++)
+            {
+                //J(2n) = 2*J(n)-1
+                //J(2n+1) = 2*J(n)+1
+                if(n % 2 == 0)
+                    remaining[n] = 2*remaining[n/2] - 1;
+                else
+                    remaining[n] = 2*remaining[n/2] + 1;
+            }
+            return remaining[elvesCount];
+        }
+
+        private static void Part2(int elvesCount)
+        {
+            Console.WriteLine(ExtendedFlawius(elvesCount));
+        }
+
+        private static int ExtendedFlawius(int elvesCount)
+        {
+            int[] extendedRemaining = new int[elvesCount+1];
+            extendedRemaining[1] = 1;
+            for(int n = 2; n <= elvesCount; n++)
+            {
+                int rem = n / 2;
+                extendedRemaining[n] = (extendedRemaining[n-1] < rem ? extendedRemaining[n-1] : extendedRemaining[n-1]+1) % n + 1;
+            }
+            return extendedRemaining[elvesCount];
+        }
+    }
+}
